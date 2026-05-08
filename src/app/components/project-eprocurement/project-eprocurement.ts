1bb8350e879bd18a6e6d6a12d3d8953a5e0f2560
Wachira Temjit <jimmyy551@gmail.com>
2026-05-08 21:51:11 +0700

    2

--- src/app/components/project-eprocurement/project-eprocurement.ts
+++ src/app/components/project-eprocurement/project-eprocurement.ts
@@ -10,5 +10,34 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './project-eprocurement.html',
   styleUrls: ['./project-eprocurement.css']
 })
+
 export class ProjectEprocurementComponent {
+  images = [
+    '/img/eprocurement/E-ProcurementSystem.png',
+    '/img/eprocurement/1.jpeg',
+    '/img/eprocurement/2.jpeg',
+  ];
+  isPreviewActive = false;
+  currentIndex = 0;
+  openPreview(index: number, event: Event) {
+    event.stopPropagation();
+    this.currentIndex = index;
+    this.isPreviewActive = true;
+  }
+  closePreview() {
+    this.isPreviewActive = false;
+  }
+  nextImage(event: Event) {
+    event.stopPropagation();
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage(event: Event) {
+    event.stopPropagation();
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+  get previewImage() {
+    return this.images[this.currentIndex];
+  }
 }
+
